b81f18fb37b90a6e3a16fef58f73876e2e7eeeac
Sean Uchida <seankuchida@gmail.com>
2022-11-24 09:42:29 -0300

    fix: react unique key error <li>

--- src/components/Home/NewFeatures.js
+++ src/components/Home/NewFeatures.js
@@ -19,32 +19,32 @@ align-self: center;
 `;
 
 function NewFeatures(props) {
-  // TODO: montar um esquema disso
-  const features = ['automatizar quotes',
+	// TODO: montar um esquema disso
+	const features = ['automatizar quotes',
 					  'adicionar mais exercícios',
 					  'permitir download dos textos',
 					  'montar nuvem de palavras',
 					  'sugestões'];
-  return (
-    <LayoutNewFeatures theme={props.theme}>
-      <FeaturesHeader theme={props.theme}>
-        Funcionalidades
-      </FeaturesHeader>
-      <LayoutList>
-        {features.map((e) => (
-          <li>
-            <LinkFeatures
-              theme={props.theme}
-              href="http://www.google.com.br"
-            >
-              {e}
-            </LinkFeatures>
-          </li>
-        ))}
-      </LayoutList>
-    </LayoutNewFeatures>
-  );
-  // TODO: colocar uma caixa de sugestões
+	return (
+			<LayoutNewFeatures theme={props.theme}>
+			<FeaturesHeader theme={props.theme}>
+			Funcionalidades
+		</FeaturesHeader>
+			<LayoutList>
+			{features.map((e) => (
+					<li key={e}>
+					<LinkFeatures
+				theme={props.theme}
+				href="http://www.google.com.br"
+					>
+					{e}
+				</LinkFeatures>
+					</li>
+			))}
+		</LayoutList>
+			</LayoutNewFeatures>
+	);
+	// TODO: colocar uma caixa de sugestões
 }
 
 export default NewFeatures;
