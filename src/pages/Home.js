5239df72d1c52cb3c17150dbd192bca53ab4d315
Sean Uchida <seankuchida@gmail.com>
2022-11-26 19:08:45 -0300

    landing page/home remake

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,33 @@ import ThemeContext from "../contexts/ThemeContext";
 import QuoteBox from "../components/Home/QuoteBox";
 import ActivityWidget from "../components/Home/ActivityWidget";
 import NewFeatures from "../components/Home/NewFeatures";
+import Hero from "../components/Home/Hero";
 
 
 // TODO: mudar o layout pra ficar responsivo
 // deixar as colunas pra 3 2 e 1
 const LayoutHome = styled.div`
 width: 100vw;
-padding-left: 2rem;
-padding-right: 2rem;
+padding-left: 3rem;
+padding-right: 3rem;
 display: flex;
 flex-direction: column;
+justify-content: space-evenly;
 
+`;
+
+const LayoutLanding = styled.div`
+width: 100vw;
+padding-left: 3rem;
+padding-right: 3rem;
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+
+`;
+
+const HomeImage = styled.img`
+max-width: 40vw;
 `;
 
 interface quoteapi {
@@ -80,19 +96,30 @@ function UserWidgets(props) {
 	}
 }
 
+
 function Home() {
 	const theme = useContext(ThemeContext);
-	
-	return(
-		<LayoutHome>
-			<GreetUser theme={theme} />
-			<QuoteBox theme={theme}>
+	const auth = useContext(AuthContext);
+	if(!auth.signed) {
+		return(
+				<LayoutLanding>
+				<Hero theme={theme} />
+				<HomeImage src="undraw_exams_re_4ios.svg" />
+			</LayoutLanding>
+		);
+	} else {
+		return(
+				<LayoutHome>
+				<GreetUser theme={theme} />
+				<QuoteBox theme={theme}>
 			    But web browsers aren’t like web servers. If your back-end code is getting so big that it’s starting to run noticably slowly, you can throw more computing power at it by scaling up your server. That’s not an option on the front-end where you don’t really have one run-time environment—your end users have their own run-time environment with its own constraints around computing power and network connectivity.
-			</QuoteBox>
-			<NewFeatures theme={theme} />
-			<UserWidgets theme={theme} />
-		</LayoutHome>
-	);
+				</QuoteBox>
+				<NewFeatures theme={theme} />
+				<UserWidgets theme={theme} />
+				</LayoutHome>
+		);
+	}
 }
+
 
 export default Home;
