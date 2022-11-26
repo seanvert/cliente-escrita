aa2ce3b0332d131389cd9ae6f3bfd068cb60de86
Sean Uchida <seankuchida@gmail.com>
2022-11-25 21:09:05 -0300

    fetchin exercises from API and loading state

--- src/pages/Trilha.js
+++ src/pages/Trilha.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import ThemeContext from "../contexts/ThemeContext";
 import ExerciseCard from "../components/Trilha/ExerciseCard";
+import AuthContext from "../contexts/AuthContext";
+
 /* tela que vai seguir basicamente a organização do livro */
 /* mostrar o progresso nas atividades */
 // acho que faz algum sentido fazer algo semelhante ao duolingo
@@ -12,37 +14,6 @@ interface Exercise {
 }
 
 // FIXME isto daqui vai sair da API
-const atividades = [
-	{
-		name: "escrita automática",
-		contents: "stub da descri~ao da atividade.",
-	},
-	{
-		name: "associaqwerqwerção livre",
-	},
-	{
-		name: "fluxo verbalasd",
-	},
-	{
-		name: "mais coisasasd",
-	},
-	{
-		name: "mais outra coisasdasda",
-	},
-	{
-		name: "associação liasdsavre",
-	},
-	{
-		name: "fluxo vasderbal",
-	},
-	{
-		name: "mais casdasdoisas",
-	},
-	{
-		name: "mais outasdasdsadra coisa",
-	},
-];
-
 
 const LayoutTrilha = styled.div`
 width: 100vw;
@@ -55,18 +26,69 @@ padding-top: 1rem;
 `;
 
 function Trilha () {
-	// componente com a visão geral da página
-	return (
-		<ThemeContext.Consumer>
-			{theme => 
-				<LayoutTrilha theme={theme} id="organizacaoTrilha">
-					{atividades.map((atividade, index) =>
-						<ExerciseCard key={atividade.name}
-									   exercise={atividade} />)}
-				</LayoutTrilha>
-			}
-		</ThemeContext.Consumer>
-	);
+	const [loading, setLoading] = useState(true);
+	const exerciseID = '';
+	const [exercises, setExercises] = useState([]);
+	const auth = useContext(AuthContext);
+	
+	const url = "http://192.168.0.100:8000/exercises";
+
+	const headersreq = {'Content-Type': 'application/x-www-form-urlencoded',
+						'Upgrade-Insecure-Requests': '1',};
+
+	useEffect(() => {
+		// TODO: user configs add 
+		var api = fetch(url, {
+			method: 'GET',
+			mode: 'cors',
+			credentials: 'include',
+			headers: headersreq,
+			redirect: 'follow',
+			referrerPolicy: 'no-referrer',
+		})
+			.then((response) => 
+				response.json())
+			.then((body) => {
+				setExercises(body.response);
+				setLoading(false);
+			});
+
+	}, [loading]);
+
+	if (loading) {
+		return (
+			<h1>Carregando...</h1>
+		);
+	} else {
+
+		return (
+			<ThemeContext.Consumer>
+				{theme => 
+					<LayoutTrilha theme={theme} id="organizacaoTrilha">
+						{exercises.map((exercise, index) => {
+
+							if (auth.signed) {
+								// TODO: isso daqui vai pro backend
+								// auth.user.config[index] = {
+								// 	exercise: atividade._id,
+								// 	config: {
+								// 		completed: false,
+								// 		time: atividade.defaultConfigs.time,
+								// 		default: false,
+								// 	}
+								// };
+							}
+							return <ExerciseCard
+									   key={exercise._id}
+									   index={index}
+									   exercise={exercise} />
+						})}
+					</LayoutTrilha>
+				}
+			</ThemeContext.Consumer>
+		);
+	}
+	
 };
 
 export default Trilha;
