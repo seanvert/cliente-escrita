2c2fb20f36d0f6c0a421ff737a8bc67b78ae7752
Sean Uchida <seankuchida@gmail.com>
2022-11-25 21:06:29 -0300

    QuoteBox fetching server API and loading state

--- src/components/Home/QuoteBox.js
+++ src/components/Home/QuoteBox.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useState, useEffect, useContext } from "react";
 
 const Quote = styled.blockquote`
 margin: 0px;
@@ -8,6 +9,7 @@ padding: 2rem;
 font-family: 'Libre Baskerville', serif;
 `;
 
+// TODO: consertar a foto de fundo
 const LayoutQuote = styled.div`
 display: grid;
 background-image: url("https://images6.fanpop.com/image/photos/37200000/Blink-Clarice-Ferguson-Wallpaper-x-men-37207678-1920-1080.jpg");
@@ -32,31 +34,62 @@ padding-right: 2rem;
 `;
 
 function QuoteFigureCaption(props) {
-  return (
-    <FigCaption>
-      &mdash; Jeremy Keith,
-      {' '}
-      <cite>Mental models</cite>
-    </FigCaption>
-  );
+	return (
+			<FigCaption>
+			&mdash; Jeremy Keith,
+		{' '}
+			<cite>Mental models</cite>
+			</FigCaption>
+	);
 }
 
 function QuoteBox(props) {
-  return (
-    <LayoutQuote theme={props.theme}>
+	const [loading, setLoading] = useState(true);
+	const url = "http://192.168.0.100:8000/quotes";
+	const [quote, setQuote] = useState({});
+	const headersreq = {'Content-Type': 'application/x-www-form-urlencoded',
+						'Upgrade-Insecure-Requests': '1',};
 
-      <QuoteFigure theme={props.theme}>
-        <Quote theme={props.theme}>
-          {props.children}
-        </Quote>
-        <QuoteFigureCaption
-          theme={props.theme}
-          source={props.source}
-        />
+	useEffect(() => {
+		// TODO: user configs add 
+		var api = fetch(url, {
+			method: 'GET',
+			mode: 'cors',
+			credentials: 'include',
+			headers: headersreq,
+			redirect: 'follow',
+			referrerPolicy: 'no-referrer',
+		})
+			.then((response) => 
+				response.json())
+			.then((body) => {
+				setQuote(body.response);
+				setLoading(false);
+			});
 
-      </QuoteFigure>
-    </LayoutQuote>
-  );
+	}, [loading]);
+
+	if (loading) {
+		// TODO: trocar isto para uma caixa do mesmo estilo da quote
+		return (
+				<h1>Carregando...</h1>
+		);
+	} else {
+		return (
+				<LayoutQuote theme={props.theme}>
+				<QuoteFigure theme={props.theme}>
+				<Quote theme={props.theme}>
+				{quote.quote}
+			</Quote>
+				<QuoteFigureCaption
+			theme={props.theme}
+			source={quote.source}
+				/>
+
+			</QuoteFigure>
+				</LayoutQuote>
+		);
+	}
 }
 
 export default QuoteBox;
