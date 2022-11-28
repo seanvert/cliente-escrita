5eaaabcee47a5cd84bd0837977b77a58dfd2b47f
Sean Uchida <seankuchida@gmail.com>
2022-11-27 21:20:32 -0300

    navbar layout fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import ThemeContext from "../contexts/ThemeContext";
 import AuthContext from "../contexts/AuthContext";
 
-const NavStyle = styled.div`
+const LayoutNavBar = styled.div`
 color: ${props => props.theme.highlight};
-display: grid;
-grid-template-columns: 1fr 1fr 1fr 1fr 2fr 1fr;
+display: flex;
+flex-directon: row;
 min-height: 10vh;
 padding-left: 2vw;
 padding-right: 2vw;
@@ -49,7 +49,8 @@ min-height: 10vh;
 
 const Navbar: React.FC = (props) => {
 	const auth = useContext(AuthContext);
-
+	const theme = useContext(ThemeContext);
+	
 	function loginLogoutBar (auth, theme) {
 		if (!auth.signed) {
 			return(
@@ -64,38 +65,59 @@ const Navbar: React.FC = (props) => {
 					<LinkStyle
 				onClick={auth.Logout}
 				theme={theme}
-				to="/">Logout
+				>Logout
 				</LinkStyle>
 					</LoginLogoutBar>
 			);
 		}
 	}
-    return (
-			<ThemeContext.Consumer>
-			{theme => 
-					<NavStyle theme={theme}>
-					<LinkStyle theme={theme} to="/">
-					Home
-				</LinkStyle>
-					
+
+	function renderNavBarLoginRoutes (auth, theme) {
+		if (auth.signed) {
+			return (
 					<LinkStyle theme={theme} to="/trilha">
 					Trilha
 				</LinkStyle>
+			);
+		} else {
+			return null;
+		}
+	}
+
+	function navBarLogoAuthRoute (auth, theme) {
+		if (auth.signed) {
+			return(
+				<LinkStyle theme={theme} to="/escrita">
+				<LogoStyle src="logotipo.svg" />
+					</LinkStyle>
+			);
+		} else {
+			return(
+				<LinkStyle theme={theme} to="/">
+				<LogoStyle src="logotipo.svg" />
+					</LinkStyle>
+			);
+		}
+	};
+	
+    return (
+					<LayoutNavBar theme={theme}>
+					<LinkStyle theme={theme} to="/">
+					Home
+				</LinkStyle>
+					{renderNavBarLoginRoutes(auth, theme)}
+
 					
 					<Spacer>
 					</Spacer>
 					
-					<LinkStyle theme={theme} to="/escrita">
-					<LogoStyle src="logotipo.svg" />
-					</LinkStyle>
+				{navBarLogoAuthRoute(auth, theme)}
 					
 					<Spacer>
 					</Spacer>
 					
 					{loginLogoutBar(auth, theme)}
-				</NavStyle>
-			}
-		</ThemeContext.Consumer>
+				</LayoutNavBar>
     );
 };
 
