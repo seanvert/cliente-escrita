04692eebb9e52137aa2a5bf3a31fd3c54ff2b6d5
Sean Uchida <seankuchida@gmail.com>
2023-09-04 12:06:18 -0300

    sub do popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const LayoutPopup = styled.div`
 color: ${props => props.theme.highlight};
 `
 // TODO adicionar isto no escrita
-const Popup: React.FC = (props) => {
-    return (
-        <LayoutPopup>
-
-        </LayoutPopup>
-    );
+const Popup: React.FC = ({ text }) => {
+    const [visible, setVisible] = useState(true);
+    if (visible) {
+        return (
+            <LayoutPopup>
+                text
+            </LayoutPopup>
+        );
+    }
 }
 
 export default Popup;
